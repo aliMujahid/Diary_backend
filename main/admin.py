a30acd850b375bcc6d0153acbc3f549eb8444f1a
aliMujahid <mhmmdalimujahid@gmail.com>
2023-11-23 04:43:58 +0600

    Topic and Entry models added

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic, Entry
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {"slug": ["name",]}
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'created', 'updated')
+    list_filter = ('topic', 'created', 'updated')
--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """Topics are used to organize the entries."""
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    """Entries are writtings on some topic"""
+
+    topic = models.ForeignKey(
+        Topic, on_delete=models.CASCADE, related_name='entries')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    text = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        return self.topic.name + self.created.strftime("%m/%d/%Y, %H:%M:%S")
