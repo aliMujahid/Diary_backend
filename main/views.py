6690a50d8d00daee03f59092dec94ba2ad563e89
aliMujahid <mhmmdalimujahid@gmail.com>
2023-11-23 05:27:03 +0600

    create_topic and add_entry view

--- main/views.py
+++ main/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import CreateTopicForm, AddEntryForm
+
 
 def index(request):
     return HttpResponse('Hello world')
+
+
+def create_topic(request):
+    if request.method == "post":
+        form = CreateTopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+def add_entry(request):
+    if request.method == "post":
+        form = AddEntryForm(request.POST)
+        if form.is_valid():
+            form.save()
