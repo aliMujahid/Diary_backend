e8f9e57868ba6007e4c19865f33133cf5b0569a9
aliMujahid <mhmmdalimujahid@gmail.com>
2023-11-23 04:47:22 +0600

    unique constrain on topic name and slug"

--- main/models.py
+++ main/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Topic(models.Model):
     """Topics are used to organize the entries."""
-    name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
